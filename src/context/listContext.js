7305358ff179e9adfd882ccc4082a60ca6d2eb47
Akshay Mahjan <maha0134@algonquinlive.com>
2022-10-16 19:48:35 -0400

    changed default quantity to one

--- src/context/listContext.js
+++ src/context/listContext.js
@@ -17,7 +17,7 @@ function ListProvider(props) {
     const listItem = {
       name: form.name.value,
       storeName: form.storeName.value,
-      quantity: form.quantity.value ? form.quantity.value : 0,
+      quantity: form.quantity.value ? form.quantity.value : 1,
     };
     if (editItemIndex === null) {
       //saving a new form
